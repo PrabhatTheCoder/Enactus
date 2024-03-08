36b7917987f549f6d8455169528c7587d29731ff
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2024-03-09 00:01:29 +0530

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -34,8 +34,8 @@ SECRET_KEY = get_random_secret_key()
 DEBUG = os.environ.get("DEBUG", "false").lower() == "true"
 # DEBUG = os.environ.get("DEBUG","False").lower() == True 
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-# ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

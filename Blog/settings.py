8f54d44c9e400cae1e1406188173486158c93a33
PrabhatTheCoder <117260172+PrabhatTheCoder@users.noreply.github.com>
2024-03-08 22:37:29 +0530

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -89,12 +89,12 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 DATABASES = {
 	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
